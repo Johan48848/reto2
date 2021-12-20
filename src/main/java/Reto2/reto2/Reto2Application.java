adb8878516b471bea8878afee84dd8196f00cbae
Johan48848 <johan.gonzalez.mt@correo.usa.edu.co>
2021-12-19 21:17:42 -0500

    first commit

---
+++ src/main/java/Reto2/reto2/Reto2Application.java
@@ -0,0 +1,27 @@
+package Reto2.reto2;
+
+import Reto2.reto2.interfaces.InterfaceCleaningproducts;
+import Reto2.reto2.interfaces.InterfaceUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@Component
+@SpringBootApplication
+public class Reto2Application implements CommandLineRunner{
+    @Autowired
+    private InterfaceCleaningproducts interfaceCleaningproducts;
+    @Autowired
+    private InterfaceUser interfaceUser;
+	public static void main(String[] args) {
+		SpringApplication.run(Reto2Application.class, args);
+	}
+         @Override
+    public void run(String... args) throws Exception {
+        interfaceCleaningproducts.deleteAll();
+        interfaceUser.deleteAll();
+    }
+        
+}
